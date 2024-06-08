3a04beef2d0d98a28ed81fb303e14059dcf35b2b
Tejas-Chaukhande <121047760+Tejas-Chaukhande@users.noreply.github.com>
2024-06-08 22:46:39 +0530

    strings-assignment.py

---
+++ strings-assignment.py
@@ -0,0 +1,20 @@
+"""
+Write a Program to retrieve character 'X' using positive indexing
+from "Semantic segmentation of Lung Cancer from Chest X-Ray" string
+"""
+str="Semantic segmentation of Lung Cancer from Chest X-Ray"
+print(str[48])
+--------------------------------------------------------------------
+"""
+Write a Program to retrieve the 'L' character from "I have to Love Python Programming" string
+using negative/reverse indexing
+"""
+str1="I have to Love Python Programming"
+print(str1[10])
+
+-------------------------------------------------------------------
+"""
+Can prove that python string is immutable with the program
+"""
+str2="Hello"
+str2[0]="M"  #modification of string not allowed as srings are immutable
